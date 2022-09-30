5a5a32de22c6a5642645bad5fa2b852d5f1393d6
saknarin <saknarin.ccs57@ubru.ac.th>
2022-09-30 08:46:35 +0700

    fix

---
+++ pages/blog/[...slug].tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+
+const BlogPostsPage: NextPage = () => {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  return (
+    <div>
+      <h1>The Blog Posts</h1>
+    </div>
+  );
+};
+
+export default BlogPostsPage;
---
+++ pages/clients/[id]/[clientprojectid].tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+
+const SelectedClientProjectPage: NextPage = () => {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  return (
+    <div>
+      <h1> The Project Page for a Specific Project for a Selected Client</h1>
+    </div>
+  );
+};
+
+export default SelectedClientProjectPage;
